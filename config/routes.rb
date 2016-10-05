c2085e4c8e38a0cb0ae018a36fe0a166e70ce9d5
Preethikasd <mspreethika@gmail.com>
2016-10-05 12:01:03 +0800

    http post and get request done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root to: 'stores#index'
+  match '/' => 'stores#create', via: :post
+  match '/:id' => 'stores#getRequest', via: :get
+  match '/' => 'stores#getRequest', via: :get
+
 end
